ae22a5fa056e619e2a862767fb79d8b4e955ce1a
jyezy <webjyezy@gmail.com>
2023-12-18 22:17:42 +1100

    Update remove seed icon

--- src/components/Modals/CreatePDA.svelte
+++ src/components/Modals/CreatePDA.svelte
@@ -6,8 +6,15 @@
   import { createEventDispatcher } from "svelte";
   const dispatch = createEventDispatcher();
   let seedsTypes = [
-    "String","Pubkey","Bytes","u8","u16","u32","u64","u128",
-  ]
+    "String",
+    "Pubkey",
+    "Bytes",
+    "u8",
+    "u16",
+    "u32",
+    "u64",
+    "u128",
+  ];
   let selectedSeed = null;
   let seeds = [];
   const addSeed = (e) => {
@@ -15,20 +22,28 @@
     // reset the selected value
     selectedSeed = null;
   };
-
-  
 </script>
 
 <h1 class="modal--title">Create PDA</h1>
 <div class="modal--form">
   <div class="modal--form-title">Seeds</div>
   {#each seeds as seed, index}
-    <div class="modal--form-seed" style="display: flex; justify-content: space-between; align-items: center; margin-bottom:0.5rem;">
-      <input class="input--primary" type={seed} placeholder={seed}/>
-      <div class="text-lava-error" on:click={()=>{seeds = seeds.filter((s, i)=>i!=index)}}>X</div>
+    <div
+      class="modal--form-seed"
+      style="display: flex; position:relative; justify-content: space-between; align-items: center; margin-bottom:0.5rem;"
+    >
+      <input class="input--primary" type={seed} placeholder={seed} />
+      <div
+        class="remove--seed"
+        on:click={() => {
+          seeds = seeds.filter((s, i) => i != index);
+        }}
+      >
+        <img src="./remove.svg" alt="Remove" />
+      </div>
     </div>
   {/each}
-<!--dropdow with an option to add each type of seed-->
+  <!--dropdow with an option to add each type of seed-->
   <Select
     class="modal--form-select"
     bind:value={selectedSeed}
@@ -38,5 +53,10 @@
   />
 </div>
 <div class="btns--modal">
-  <button class="btn btn--lava" on:click={()=>{console.log("pp")}}>Create</button>
+  <button
+    class="btn btn--lava"
+    on:click={() => {
+      console.log("pp");
+    }}>Create</button
+  >
 </div>
