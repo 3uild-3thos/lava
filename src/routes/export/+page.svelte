c367c15571d2d6bc3eb5f87788af385b82c11a1b
JuanMarchetto <juanpatriciomarchetto@gmail.com>
2024-03-09 13:37:24 -0300

    trying another aproach

--- src/routes/export/+page.svelte
+++ src/routes/export/+page.svelte
@@ -13,7 +13,7 @@
   import "prismjs/components/prism-json";
   let code = JSON.stringify($workspaces[$selectedWorkspace], null, 2);
   import { copy } from "svelte-copy";
-    import { goto } from "$app/navigation";
+    import { invalidateAll } from "$app/navigation";
 
   let workspace = $workspaces[$selectedWorkspace];
   let selectedProgramIndex = -1;
@@ -22,8 +22,8 @@
   onMount(async () => {
     init().then(() => {
       templates = get_templates();
-    }).catch((e) => {
-      goto(window.location.pathname);
+    }).catch(async (e) => {
+      await invalidateAll();
     })
     ready = true;
     initSync();
